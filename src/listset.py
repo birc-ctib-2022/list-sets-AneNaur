af80be17b8701a2fb8a3b392020fe708984b787e
AneNaur <202008164@post.au.dk>
2022-11-21 14:09:55 +0100

    listset?

--- src/listset.py
+++ src/listset.py
@@ -15,10 +15,17 @@ class ListSet(Generic[T]):
     def __init__(self, init: Iterable[T]) -> None:
         """Initialise set with init."""
         ...  # FIXME
+        self.data = init
+    #Running time = lineær/O(n)?
 
     def __contains__(self, x: T) -> bool:
         """Test if x is in set."""
         ...  # FIXME
+        if x in self.data:
+            return True
+        else:
+            return False
+    #in-funktionen laver en lineær search og derfor Running time = lineær/O(n)
 
     def __bool__(self) -> bool:
         """
@@ -28,11 +35,20 @@ class ListSet(Generic[T]):
         otherwise
         """
         ...  # FIXME
+        return bool(self)
+    #bool-funktionen har Running time = konstant/O(1)?
 
     def add(self, x: T) -> None:
         """Add x to the set."""
         ...  # FIXME
+        if x not in self:
+            self.data.append(x)
+    #not in-funktionen lineær search mens append har konstant kompleksitet, altså Running time = lineær/O(n)?
 
     def remove(self, x: T) -> None:
         """Remove x from the set."""
         ...  # FIXME
+        if x in self:
+            idx = self.data.index(x)
+            self.data.pop(idx)
+    #in-funktioner laver lineær search, og pop-funktionen skal rykke alle elementer efter idx, så Running time = kvadratisk/O(n^2)?
